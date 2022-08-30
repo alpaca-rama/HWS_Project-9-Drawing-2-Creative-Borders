7bc120e3e1a4b58a9368844d13572ae8abc8b0e7
Luca <luca@weve-got-it.com>
2022-08-30 11:48:33 +0200

    Added content: Lesson -> Creative borders and fills using ImagePaint.

--- Project-9-Drawing-2-Creative-Borders/ContentView.swift
+++ Project-9-Drawing-2-Creative-Borders/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("singapore"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
